d9ce194cdd7392c76e89dd30aa0bab38fd91865a
Letícia Keuffer <lekeuffer@gmail.com>
2026-04-08 19:29:20 -0300

    VO, exception, ajustes

---
+++ DDD_Api_Base/Common/Domain/Exceptions/InvalidEventSpotException.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Domain.Exceptions
+{
+    public class InvalidEventSpotException : Exception
+    {
+        public InvalidEventSpotException(string mensagem) : base(mensagem) { }
+    }
+}
